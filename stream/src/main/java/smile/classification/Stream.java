4e2b2eb45b37457330c7cbc75e4da657add4c077
Joanna Roczniak <roczniak.joanna@gmail.com>
2016-11-09 02:09:26 +0100

    Improve logs to better understanding the data mining. Improve DDM - better probability's calculation. Add concept drift detection for decision tree. Save log outputs into html Test Results.

--- stream/src/main/java/smile/classification/Stream.java
+++ stream/src/main/java/smile/classification/Stream.java
@@ -1,5 +1,13 @@
 package smile.classification;
 
+import smile.data.AttributeDataset;
+import smile.data.NominalAttribute;
+import smile.data.parser.DelimitedTextParser;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.ParseException;
+
 /**
  * Created by joanna on 11/3/16.
  * Stream is a class, which imitates streaming data
@@ -18,8 +26,11 @@ public class Stream {
     private int warningIndex = -1;
     private int numberOfDrifts = 0;
 
-    Stream(int maxWindowSize) {
+    private String algorithmType = "";
+
+    Stream(int maxWindowSize, String algorithmType) {
         this.maxWindowSize = maxWindowSize;
+        this.algorithmType = algorithmType;
     }
 
     public void start(double[][] x, int[] y) {
@@ -29,12 +40,11 @@ public class Stream {
         int iterator;
 
         //fill the window with the data for the first time to avoid NullPointerException
-        for (iterator = 0; iterator < 10; iterator++) {
+        for (iterator = 0; iterator < 100; iterator++) {
             window.add(x[iterator], y[iterator]);
         }
 
         while (true) {
-            System.out.println("***" + iterator + "***");
             //check if we finished processing the data array
             if (iterator == y.length) {
                 System.out.println("Number of drifts: " + numberOfDrifts);
@@ -44,15 +54,22 @@ public class Stream {
             if (driftDetected) {
                 //in case of drift
                 window.changeInto(possibleWindow);
-                possibleWindow.clear();
                 numberOfDrifts++;
+
+                System.out.println("RESET all stored constants");
+                possibleWindow.clear();
+
                 minProbability = Double.MAX_VALUE;
                 minDeviation = Double.MAX_VALUE;
 
                 iterator = warningIndex;
                 driftDetected = false;
+                warningIndex = -1;
+            } else {
+                System.out.println("***" + iterator + "***");
+                update(x[iterator], y[iterator], iterator);
+                ++iterator;
             }
-            update(x[iterator], y[iterator], iterator++);
         }
     }
 
@@ -65,60 +82,84 @@ public class Stream {
 
         //learn the algorithm and check the error rate of the data in window
         try {
-            int error = learnAndGetErrorRate(window);
+            int error = learnAndGetErrorRate(window, algorithmType);
             //check the Concept Drift
 
-            double probabilityOfMisclassifying = (error * 100) / maxWindowSize;
-            probabilityOfMisclassifying /= 100;
+            double probabilityOfMisclassifying = 100.0 * error / window.getIterator();
+//            probabilityOfMisclassifying /= 100;
 
             //there should be also divided by t - ilość próbek
-            double standardDeviation = Math.sqrt((probabilityOfMisclassifying * (1 - probabilityOfMisclassifying)));
+            double standardDeviation = Math.sqrt((probabilityOfMisclassifying * (1 - probabilityOfMisclassifying)) / window.getIterator());
 
             //checking the min probability and deviation
             if (probabilityOfMisclassifying < minProbability) {
                 minProbability = probabilityOfMisclassifying;
-                System.out.println("MIN probability: " + minProbability);
+                System.out.format("MIN probability: %.5f%%%n", minProbability);
             }
             if (standardDeviation < minDeviation) {
                 minDeviation = standardDeviation;
-                System.out.println("MIN deviation: " + minDeviation);
+                System.out.format("MIN deviation: %.5f%%%n", minDeviation);
             }
 
-            //checking the warning and drift level
-            if (probabilityOfMisclassifying + standardDeviation >= minProbability + 2 * minDeviation) {
-                System.out.println("WARNING LEVEL");
-                System.out.println("Standard deviation: " + standardDeviation);
-                System.out.println("Probability of misclassifying: " + probabilityOfMisclassifying);
-                warningIndex = iterator;
-                //keep data from warning level
-                possibleWindow.add(x, y);
+            //check the first occurrence of warning level
+            if (warningIndex == -1) {
+                if (probabilityOfMisclassifying + standardDeviation >= minProbability + 2 * minDeviation) {
+                    System.out.format("Probability of misclassifying: %.5f%%%n", probabilityOfMisclassifying);
+                    System.out.format("Standard deviation: %.5f%%%n", standardDeviation);
+                    System.out.println("WARNING LEVEL");
+                    warningIndex = iterator;
+                    //store data from a warning level
+                    possibleWindow.add(x, y);
+                }
             }
             if (probabilityOfMisclassifying + standardDeviation >= minProbability + 3 * minDeviation) {
+                System.out.format("Probability of misclassifying: %.5f%%%n", probabilityOfMisclassifying);
+                System.out.format("Standard deviation: %.5f%%%n", standardDeviation);
                 System.out.println("DRIFT LEVEL");
-                System.out.println("Standard deviation: " + standardDeviation);
-                System.out.println("Probability of misclassifying: " + probabilityOfMisclassifying);
-                //set window into possibleWindow
+                //concept drift detected
                 driftDetected = true;
-
             }
         } catch (IllegalArgumentException e) {
             return;
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
         }
     }
 
-    private int learnAndGetErrorRate(Window window) throws IllegalArgumentException {
+
+    private int learnAndGetErrorRate(Window window, String algorithmType) throws IllegalArgumentException, IOException, ParseException {
+
+        int error = 0;
         double[][] newX = window.getX();
         int[] newY = window.getY();
 
-        int error = 0;
-
-        if (newX != null && newY != null) {
-            KNN<double[]> knn = KNN.learn(newX, newY, 15); // the number indicates the number of neighbours
-            for (int i = 0; i < newY.length; i++) {
-                if (knn.predict(window.getX()[i]) != window.getY()[i]) {
-                    error++; //set the error rate in the error array
+        switch (algorithmType) {
+            case "knn":
+                if (newX != null && newY != null) {
+                    KNN<double[]> knn = KNN.learn(newX, newY, 15); // the number indicates the number of neighbours
+                    for (int i = 0; i < newY.length; i++) {
+                        if (knn.predict(window.getX()[i]) != window.getY()[i]) {
+                            error++; //set the error rate in the error array
+                        }
+                    }
                 }
-            }
+                break;
+
+            case "tree":
+                DelimitedTextParser parser = new DelimitedTextParser();
+                parser.setResponseIndex(new NominalAttribute("class"), 0);
+                AttributeDataset test = parser.parse("USPS Test", new FileInputStream("/home/joanna/IdeaProjects/smiling/shell/src/universal/data/usps/zip.test"));
+                double[][] testx = test.toArray(new double[test.size()][]);
+                int[] testy = test.toArray(new int[test.size()]);
+
+                DecisionTree tree = new DecisionTree(newX, newY, 350, DecisionTree.SplitRule.ENTROPY);
+
+                for (int i = 0; i < testx.length; i++) {
+                    if (tree.predict(testx[i]) != testy[i]) {
+                        error++;
+                    }
+                }
+                break;
         }
         return error;
     }
@@ -183,6 +224,10 @@ public class Stream {
                 this.y[i] = newWindow.getY()[i];
                 System.arraycopy(newWindow.getX()[i], 0, this.x[i], 0, x[i].length);
             }
+        }
+
+        int getIterator() {
+            return iterator;
         }
     }
 }
--- stream/src/test/java/smile/classification/StreamTest.java
+++ stream/src/test/java/smile/classification/StreamTest.java
@@ -6,6 +6,8 @@ import smile.data.NominalAttribute;
 import smile.data.parser.DelimitedTextParser;
 
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.text.ParseException;
 
 /**
  * Created by joanna on 11/5/16.
@@ -13,17 +15,43 @@ import java.io.FileInputStream;
  */
 public class StreamTest {
     @Test
-    public void testSea() throws Exception {
+    public void testSeaKNN() {
         DelimitedTextParser parser = new DelimitedTextParser();
         parser.setDelimiter(",");
         parser.setResponseIndex(new NominalAttribute("class"), 3);
 
-        AttributeDataset attributeDataset = parser.parse("SEA", new FileInputStream("/home/joanna/IdeaProjects/smiling/shell/src/universal/data/stream/sea.data"));
-        double[][] x = attributeDataset.toArray(new double[0][]);
-        int[] y = attributeDataset.toArray(new int[0]);
+        try {
+            AttributeDataset attributeDataset = parser.parse("SEA", new FileInputStream("/home/joanna/IdeaProjects/smiling/shell/src/universal/data/stream/sea.data"));
+            double[][] x = attributeDataset.toArray(new double[0][]);
+            int[] y = attributeDataset.toArray(new int[0]);
 
-        Stream stream = new Stream(200); //the number indicates the maxWindowSize
+            Stream stream = new Stream(300, "knn"); //the number indicates the maxWindowSize, string indicates the method used to learn the data
 
-        stream.start(x, y);
+            stream.start(x, y);
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Test of learn method, of class DecisionTree.
+     */
+    @Test
+    public void testUSPSDecisionTree() {
+        System.out.println("USPS");
+        DelimitedTextParser parser = new DelimitedTextParser();
+        parser.setResponseIndex(new NominalAttribute("class"), 0);
+        try {
+            AttributeDataset train = parser.parse("USPS Train", new FileInputStream("/home/joanna/IdeaProjects/smiling/shell/src/universal/data/usps/zip.train"));
+            double[][] x = train.toArray(new double[train.size()][]);
+            int[] y = train.toArray(new int[train.size()]);
+
+            Stream stream = new Stream(500, "tree"); //the number indicates the maxWindowSize
+
+            stream.start(x, y);
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 }
