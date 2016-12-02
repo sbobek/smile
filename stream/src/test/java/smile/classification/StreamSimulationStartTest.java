5e81b59e0ed7b395ad2af04796983788ae87198c
Joanna Roczniak <roczniak.joanna@gmail.com>
2016-12-02 01:45:42 +0100

    Add time to tests.

--- stream/src/test/java/smile/classification/StreamSimulationStartTest.java
+++ stream/src/test/java/smile/classification/StreamSimulationStartTest.java
@@ -14,44 +14,94 @@ public class StreamSimulationStartTest {
     @Test
     public void testStartHyperplaneKNN() throws FileNotFoundException {
 
-        PrintStream out = new PrintStream(new FileOutputStream("output/hyperplaneKNNStart.txt"));
+        PrintStream out = new PrintStream(new FileOutputStream("output/hyperplaneKNNStartBeta8.txt"));
         System.setOut(out);
+
+        long start = System.nanoTime();
 
         StreamSimulation simulation = new StreamSimulation("/home/joanna/IdeaProjects/smiling/shell/src/universal/data/stream/hyperplane/hyperplane1.arff", 10);
 
-        simulation.start("knn", 100, 2, 3);
+        simulation.start("knn", 100, 2, 8);
+        long elapsedTime = System.nanoTime() - start;
+
+        System.out.println("Time: " + elapsedTime);
     }
 
     @Test
     public void testStartHyperplaneTree() throws FileNotFoundException {
 
-        PrintStream out = new PrintStream(new FileOutputStream("output/hyperplaneTreeStart.txt"));
+        PrintStream out = new PrintStream(new FileOutputStream("output/hyperplaneTreeStartBeta.txt"));
         System.setOut(out);
+
+        long start = System.nanoTime();
 
         StreamSimulation simulation = new StreamSimulation("/home/joanna/IdeaProjects/smiling/shell/src/universal/data/stream/hyperplane/hyperplane1.arff", 10);
 
         simulation.start("tree", 100, 2, 3);
+        long elapsedTime = System.nanoTime() - start;
+
+        System.out.println("Time: " + elapsedTime);
     }
 
     @Test
-    public void testStartSea() throws FileNotFoundException {
+    public void testStartSeaKNN() throws FileNotFoundException {
 
-        PrintStream out = new PrintStream(new FileOutputStream("output/seaKNNStart.txt"));
+        PrintStream out = new PrintStream(new FileOutputStream("output/seaKNNStartBeta5.txt"));
         System.setOut(out);
 
+        long start = System.nanoTime();
         StreamSimulation simulation = new StreamSimulation("/home/joanna/IdeaProjects/smiling/shell/src/universal/data/stream/sea/sea.arff", 3);
 
-        simulation.start("knn", 100, 2, 3);
+        simulation.start("knn", 100, 2, 5);
+        long elapsedTime = System.nanoTime() - start;
+
+        System.out.println("Time: " + elapsedTime);
     }
 
     @Test
     public void testStartSeaTree() throws FileNotFoundException {
 
-        PrintStream out = new PrintStream(new FileOutputStream("output/seaTreeStart.txt"));
+        PrintStream out = new PrintStream(new FileOutputStream("output/seaTreeStartBeta5.txt"));
         System.setOut(out);
+        long start = System.nanoTime();
 
         StreamSimulation simulation = new StreamSimulation("/home/joanna/IdeaProjects/smiling/shell/src/universal/data/stream/sea/sea.arff", 3);
 
-        simulation.start("tree", 100, 2, 3);
+        simulation.start("tree", 100, 2, 5);
+
+        long elapsedTime = System.nanoTime() - start;
+
+        System.out.println("Time: " + elapsedTime);
+    }
+
+    @Test
+    public void testStartAirlinesKNN() throws Exception {
+
+        PrintStream out = new PrintStream(new FileOutputStream("output/airlinesKNNStart.txt"));
+        System.setOut(out);
+        long start = System.nanoTime();
+
+        StreamSimulation simulation = new StreamSimulation("/home/joanna/IdeaProjects/smiling/shell/src/universal/data/stream/airlines.arff", 7);
+
+        simulation.start("knn", 500, 2, 3);
+        long elapsedTime = System.nanoTime() - start;
+
+        System.out.println("Time: " + elapsedTime);
+    }
+
+    @Test
+    public void testStartAirlinesTree() throws Exception {
+
+        PrintStream out = new PrintStream(new FileOutputStream("output/airlinesTreeStart.txt"));
+        System.setOut(out);
+
+        long start = System.nanoTime();
+
+        StreamSimulation simulation = new StreamSimulation("/home/joanna/IdeaProjects/smiling/shell/src/universal/data/stream/airlines.arff", 7);
+
+        simulation.start("tree", 500, 2, 3);
+        long elapsedTime = System.nanoTime() - start;
+
+        System.out.println("Time: " + elapsedTime);
     }
 }
